8889c3aac6c47931c5dc426ffd3d77a64b293bea
puppywimy <eric250@naver.com>
2026-05-20 14:42:53 +0900

    19강: 댓글 RsData는 data를 비우기 위해 Void 타입 전달

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -43,7 +43,7 @@ public class ApiV1PostCommentController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public RsData<PostCommentDto> deleteItem(@PathVariable int postId, @PathVariable int id) {
+    public RsData<Void> deleteItem(@PathVariable int postId, @PathVariable int id) {
         Optional<Post> optionalPost = postService.findById(postId);
         if (optionalPost.isEmpty()) return null;
         Post post = optionalPost.get();
@@ -56,8 +56,7 @@ public class ApiV1PostCommentController {
 
         return new RsData<>(
                 "200-1",
-                "%d번 댓글이 삭제되었습니다.".formatted(comment.getId()),
-                new PostCommentDto(comment)
+                "%d번 댓글이 삭제되었습니다.".formatted(comment.getId())
         );
     }
 }
--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -1,4 +1,7 @@
 package com.back.global.rsData;
 
 public record RsData<T>(String resultCode, String msg, T data) {
+    public RsData(String resultCode, String msg) {
+        this(resultCode, msg, null);
+    }
 }
