4a6ef26161bed6a399c2ad51275036093fad74ea
puppywimy <eric250@naver.com>
2026-05-21 12:41:25 +0900

    31강: PUT /posts/{id} 추가

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,6 +3,8 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.domain.post.postComment.dto.PostCommentDto;
+import com.back.domain.post.postComment.entity.PostComment;
 import com.back.global.rsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -79,6 +81,35 @@ public class ApiV1PostController {
         return new RsData<>(
                 "200-1",
                 "%d번 글이 삭제되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
+    }
+
+    public record PostUpdateReqBody(
+            @NotBlank
+            @Size(min = 2, max = 20)
+            String title,
+            @NotBlank
+            @Size(min = 2, max = 5000)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public RsData<PostDto> update(
+            @PathVariable int id,
+            @RequestBody @Valid PostUpdateReqBody body
+    ) {
+        Optional<Post> optionalPost = postService.findById(id);
+        if (optionalPost.isEmpty()) return null;
+        Post post = optionalPost.get();
+
+        postService.modify(post, body.title, body.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글이 수정되었습니다.".formatted(post.getId()),
                 new PostDto(post)
         );
     }
