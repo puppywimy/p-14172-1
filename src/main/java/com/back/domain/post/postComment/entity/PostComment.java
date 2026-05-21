f77835b5394dfa295b4db35676d0c81058c0cf4a
puppywimy <eric250@naver.com>
2026-05-21 15:59:04 +0900

    fix: PostComment의 ManyToOne fetch 타입 LAZY로 설정, JsonIgnore는 DTO 도입하면서 필요없어져서 삭제

--- src/main/java/com/back/domain/post/postComment/entity/PostComment.java
+++ src/main/java/com/back/domain/post/postComment/entity/PostComment.java
@@ -2,18 +2,18 @@ package com.back.domain.post.postComment.entity;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.global.jpa.entity.BaseEntity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 @Getter
 @NoArgsConstructor
 public class PostComment extends BaseEntity {
-    @ManyToOne
-    @JsonIgnore
+    @ManyToOne(fetch = LAZY)
     private Post post;
     private String content;
 
