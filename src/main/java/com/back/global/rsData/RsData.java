cfcf65b316fc29ab3f2b4ab0c034a856a2a52bd3
puppywimy <eric250@naver.com>
2026-05-21 11:58:23 +0900

    28강: AOP를 이용해서 resultCode로 HTTP 응답 코드를 설정하도록 변경

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -1,7 +1,23 @@
 package com.back.global.rsData;
 
-public record RsData<T>(String resultCode, String msg, T data) {
-    public RsData(String resultCode, String msg) {
+public record RsData<T>(
+        String resultCode,
+        int statusCode,
+        String msg,
+        T data
+) {
+    public RsData(
+            String resultCode,
+            String msg,
+            T data
+    ) {
+        this(resultCode, Integer.parseInt(resultCode.split("-")[0]), msg, data);
+    }
+
+    public RsData(
+            String resultCode,
+            String msg
+    ) {
         this(resultCode, msg, null);
     }
 }
