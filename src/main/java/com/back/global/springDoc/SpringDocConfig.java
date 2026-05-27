1f9060c8d0dbdf890af9d85030c09a5b5db1b695
puppywimy <eric250@naver.com>
2026-05-27 10:14:08 +0900

    61강: GroupedOpenApi을 이용해서 API 버전별로 그룹화, API가 아닌 것들도 따로 모아서 그룹화

--- src/main/java/com/back/global/springDoc/SpringDocConfig.java
+++ src/main/java/com/back/global/springDoc/SpringDocConfig.java
@@ -2,6 +2,8 @@ package com.back.global.springDoc;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,4 +15,19 @@ import org.springframework.context.annotation.Configuration;
         )
 )
 public class SpringDocConfig {
+    @Bean
+    public GroupedOpenApi groupApiV1() {
+        return GroupedOpenApi.builder()
+                .group("apiV1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi groupController() {
+        return GroupedOpenApi.builder()
+                .group("home")
+                .pathsToExclude("/api/**")
+                .build();
+    }
 }
