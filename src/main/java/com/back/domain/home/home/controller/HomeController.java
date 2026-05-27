8642d9e0e889ef4ecb7fe5651fc553f5e22f5dd8
puppywimy <eric250@naver.com>
2026-05-27 10:30:29 +0900

    63강: HomeController 관련 문서 꾸미기

--- src/main/java/com/back/domain/home/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.back.domain.home.home.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,10 @@ import static java.net.InetAddress.getLocalHost;
 import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 
 @RestController
+@Tag(name = "HomeController", description = "홈 컨트롤러")
 @RequestMapping(produces = TEXT_HTML_VALUE)
 public class HomeController {
+    @Operation(summary = "메인 페이지")
     @SneakyThrows
     @GetMapping
     public String main() {
